9b249ca548c41e25f7bdc9c2eae83a3494a36248
AudrinaOrtega <audrina.ortega@fwcsstudents.org>
2025-04-25 08:55:09 -0400

    ExtremeHeat_visualization.py

--- python files/ExtremeHeat_visualization.py
+++ python files/ExtremeHeat_visualization.py
@@ -5,12 +5,16 @@ import plotly.express as px
 file_path = 'extreme heat.csv'
 df = pd.read_csv(file_path)
 
+#converts and cleans date columns,
+#and removes any rows where there isn't a date/invalid values (just incase)
 df['date'] = pd.to_datetime(df['DATE'], errors='coerce')
 df = df.dropna(subset=['date'])
 
+#creates columns for year and day/date
 df['Year'] = df['date'].dt.year
 df['Day'] = df['date'].dt.strftime('%m-%d')
 
+#makes the bar for that (bland stuff again) 
 fig = px.bar(
     df, x='Year', y='TEMP', color='Day',
     title="Extreme Heat Over the Years",
@@ -18,6 +22,7 @@ fig = px.bar(
     hover_data={'Day': True}
 )
 
+#used it for side by side comparision (looked nicer and neater along with more clarity)
 fig.update_layout(barmode='group')
 
 fig.write_html("extreme_heat.html")
